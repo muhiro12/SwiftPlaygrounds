9d212a111777fc2c4571001d5eeac12fe0660b67
muhiro_ <58671793+muhiro12@users.noreply.github.com>
2024-05-21 23:45:41 +0900

    Modify InfinitePagingViewController

--- SwiftPlaygrounds.swiftpm/Storyboard/InfinitePaging/InfinitePagingViewController.swift
+++ SwiftPlaygrounds.swiftpm/Storyboard/InfinitePaging/InfinitePagingViewController.swift
@@ -5,32 +5,41 @@ final class InfinitePagingViewController: UIViewController {
         transitionStyle: .scroll,
         navigationOrientation: .horizontal
     )
-    private let numbers = 0..<5
+
+    private lazy var viewControllers = (0..<5).map {
+        buildViewController($0)
+    }
 
     private var index = 0    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         addChild(pageViewController)
         view.addSubview(pageViewController.view)
         pageViewController.didMove(toParent: self)
-        
+
         pageViewController.dataSource = self
-        
+
         pageViewController.setViewControllers(
-            [buildViewController(numbers.startIndex)], 
+            [viewControllers[0]],
             direction: .forward,
             animated: true
-        )        
+        )
+        pageViewController.view.clipsToBounds = false
+        for view in pageViewController.view.subviews {
+            if let scrollView = view as? UIScrollView {
+                scrollView.clipsToBounds = false
+            }
+        }
         
         pageViewController.view.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            pageViewController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            pageViewController.view.topAnchor.constraint(equalTo: view.topAnchor),
-            pageViewController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            pageViewController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            pageViewController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            pageViewController.view.topAnchor.constraint(equalTo: view.topAnchor, constant: 40),
+            pageViewController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+            pageViewController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -40),
         ])
     }
     
@@ -38,13 +47,21 @@ final class InfinitePagingViewController: UIViewController {
         let vc = UIViewController()
         let label = UILabel()
         label.text = number.description
+        label.textAlignment = .center
+        label.backgroundColor = .init(red: .random(in: 0...1),
+                                      green: .random(in: 0...1),
+                                      blue: .random(in: 0...1),
+                                      alpha: 1)
+        
         vc.view.addSubview(label)
         
         label.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            vc.view.centerXAnchor.constraint(equalTo: label.centerXAnchor),
-            vc.view.centerYAnchor.constraint(equalTo: label.centerYAnchor)
+            label.widthAnchor.constraint(equalToConstant: 300),
+            label.heightAnchor.constraint(equalToConstant: 200),
+            label.centerXAnchor.constraint(equalTo: vc.view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: vc.view.centerYAnchor)
         ])
         
         return vc
@@ -54,18 +71,18 @@ final class InfinitePagingViewController: UIViewController {
 extension InfinitePagingViewController: UIPageViewControllerDataSource {
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
         index -= 1
-        if index < numbers.startIndex {
-            index = numbers.endIndex - 1
+        if index < viewControllers.startIndex {
+            index = viewControllers.endIndex - 1
         }
-        return buildViewController(numbers[index])
+        return viewControllers[index]
     }
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
         index += 1
-        if index > numbers.endIndex - 1 {
-            index = numbers.startIndex
+        if index > viewControllers.endIndex - 1 {
+            index = viewControllers.startIndex
         }
-        return buildViewController(numbers[index])
+        return viewControllers[index]
     }
 }
 
