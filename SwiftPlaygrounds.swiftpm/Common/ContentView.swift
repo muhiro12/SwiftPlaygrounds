47f614b86f6493d5d5eb3503ba94c76304b50b46
muhiro_ <58671793+muhiro12@users.noreply.github.com>
2024-08-18 07:00:07 +0900

    Add sort button to ContentView

--- SwiftPlaygrounds.swiftpm/Common/ContentView.swift
+++ SwiftPlaygrounds.swiftpm/Common/ContentView.swift
@@ -10,23 +10,37 @@ import SwiftUI
 struct ContentView: View {
     @State private var sidebar: Sidebar? = .all
     @State private var content: Route? = .allCases.last
+    
+    @State private var isAscending = false
 
     var body: some View {
         NavigationSplitView {
-            List(Sidebar.allCases, id: \.self, selection: $sidebar) {
+            List(Sidebar.allCases,
+                 id: \.self,
+                 selection: $sidebar) {
                 Text($0.title)
             }
             .navigationTitle(String(describing: type(of: self)))
         } content: {
             if let sidebar {
-                List(sidebar.contentSections, id: \.id, selection: $content) { section in
+                List(isAscending ? sidebar.contentSections : sidebar.contentSections.reversed(),
+                     id: \.id,
+                     selection: $content) { section in
                     Section(section.title) {
-                        ForEach(section.contents, id: \.self) { content in
+                        ForEach(isAscending ? section.contents : section.contents.reversed(),
+                                id: \.self) { content in
                             Text(content.title)   
                         }
                     }
                 }
                 .navigationTitle(sidebar.title)
+                .toolbar {
+                    ToolbarItem {
+                        Button("Sort", systemImage: "arrow.up.and.down.text.horizontal") {
+                            isAscending.toggle()
+                        }
+                    }
+                }
             }
         } detail: {
             if let content {
